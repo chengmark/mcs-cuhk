9598a90a40c00a09488799b8fbf80a4b860374a2
chengmark <mocheng26@gmail.com>
2021-11-02 15:11:11 +0800

    update README theme

--- theme.js
+++ theme.js
@@ -1,5 +1,4 @@
 import { createTheme, responsiveFontSizes } from '@mui/material'
-import _ from 'lodash'
 
 const typography = {
   h1: {
@@ -37,35 +36,7 @@ const typography = {
   }
 }
 
-const softShadows = [
-  'none',
-  '0 0 0 1px rgba(63,63,68,0.05), 0 1px 2px 0 rgba(63,63,68,0.15)',
-  '0 0 1px 0 rgba(0,0,0,0.31), 0 2px 2px -2px rgba(0,0,0,0.25)',
-  '0 0 1px 0 rgba(0,0,0,0.31), 0 3px 4px -2px rgba(0,0,0,0.25)',
-  '0 0 1px 0 rgba(0,0,0,0.31), 0 3px 4px -2px rgba(0,0,0,0.25)',
-  '0 0 1px 0 rgba(0,0,0,0.31), 0 4px 6px -2px rgba(0,0,0,0.25)',
-  '0 0 1px 0 rgba(0,0,0,0.31), 0 4px 6px -2px rgba(0,0,0,0.25)',
-  '0 0 1px 0 rgba(0,0,0,0.31), 0 4px 8px -2px rgba(0,0,0,0.25)',
-  '0 0 1px 0 rgba(0,0,0,0.31), 0 5px 8px -2px rgba(0,0,0,0.25)',
-  '0 0 1px 0 rgba(0,0,0,0.31), 0 6px 12px -4px rgba(0,0,0,0.25)',
-  '0 0 1px 0 rgba(0,0,0,0.31), 0 7px 12px -4px rgba(0,0,0,0.25)',
-  '0 0 1px 0 rgba(0,0,0,0.31), 0 6px 16px -4px rgba(0,0,0,0.25)',
-  '0 0 1px 0 rgba(0,0,0,0.31), 0 7px 16px -4px rgba(0,0,0,0.25)',
-  '0 0 1px 0 rgba(0,0,0,0.31), 0 8px 18px -8px rgba(0,0,0,0.25)',
-  '0 0 1px 0 rgba(0,0,0,0.31), 0 9px 18px -8px rgba(0,0,0,0.25)',
-  '0 0 1px 0 rgba(0,0,0,0.31), 0 10px 20px -8px rgba(0,0,0,0.25)',
-  '0 0 1px 0 rgba(0,0,0,0.31), 0 11px 20px -8px rgba(0,0,0,0.25)',
-  '0 0 1px 0 rgba(0,0,0,0.31), 0 12px 22px -8px rgba(0,0,0,0.25)',
-  '0 0 1px 0 rgba(0,0,0,0.31), 0 13px 22px -8px rgba(0,0,0,0.25)',
-  '0 0 1px 0 rgba(0,0,0,0.31), 0 14px 24px -8px rgba(0,0,0,0.25)',
-  '0 0 1px 0 rgba(0,0,0,0.31), 0 16px 28px -8px rgba(0,0,0,0.25)',
-  '0 0 1px 0 rgba(0,0,0,0.31), 0 18px 30px -8px rgba(0,0,0,0.25)',
-  '0 0 1px 0 rgba(0,0,0,0.31), 0 20px 32px -8px rgba(0,0,0,0.25)',
-  '0 0 1px 0 rgba(0,0,0,0.31), 0 22px 34px -8px rgba(0,0,0,0.25)',
-  '0 0 1px 0 rgba(0,0,0,0.31), 0 24px 36px -8px rgba(0,0,0,0.25)'
-]
-
-const strongShadows = [
+const shadows = [
   'none',
   '0 0 1px 0 rgba(0,0,0,0.70), 0 3px 4px -2px rgba(0,0,0,0.50)',
   '0 0 1px 0 rgba(0,0,0,0.70), 0 2px 2px -2px rgba(0,0,0,0.50)',
@@ -93,8 +64,7 @@ const strongShadows = [
   '0 0 1px 0 rgba(0,0,0,0.70), 0 24px 36px -8px rgba(0,0,0,0.50)'
 ]
 
-const baseOptions = {
-  direction: 'ltr',
+const options = {
   typography,
   overrides: {
     MuiLinearProgress: {
@@ -113,45 +83,38 @@ const baseOptions = {
         backgroundColor: 'rgba(0,0,0,0.075)'
       }
     }
-  }
+  },
+  palette: {
+    type: 'dark',
+    action: {
+      active: 'rgba(255, 255, 255, 0.54)',
+      hover: 'rgba(255, 255, 255, 0.04)',
+      selected: 'rgba(255, 255, 255, 0.08)',
+      disabled: 'rgba(255, 255, 255, 0.26)',
+      disabledBackground: 'rgba(255, 255, 255, 0.12)',
+      focus: 'rgba(255, 255, 255, 0.12)'
+    },
+    background: {
+      default: '#282C34',
+      dark: '#1c2025',
+      paper: '#282C34'
+    },
+    primary: {
+      main: '#8a85ff'
+    },
+    secondary: {
+      main: '#8a85ff'
+    },
+    text: {
+      primary: '#e6e5e8',
+      secondary: '#adb0bb'
+    }
+  },
+  shadows: shadows
 }
 
-const themeOptions = [
-  {
-    palette: {
-      type: 'dark',
-      action: {
-        active: 'rgba(255, 255, 255, 0.54)',
-        hover: 'rgba(255, 255, 255, 0.04)',
-        selected: 'rgba(255, 255, 255, 0.08)',
-        disabled: 'rgba(255, 255, 255, 0.26)',
-        disabledBackground: 'rgba(255, 255, 255, 0.12)',
-        focus: 'rgba(255, 255, 255, 0.12)'
-      },
-      background: {
-        default: '#282C34',
-        dark: '#1c2025',
-        paper: '#282C34'
-      },
-      primary: {
-        main: '#8a85ff'
-      },
-      secondary: {
-        main: '#8a85ff'
-      },
-      text: {
-        primary: '#e6e5e8',
-        secondary: '#adb0bb'
-      }
-    },
-    shadows: strongShadows
-  }
-]
-
 export const createMyTheme = (config = {}) => {
-  let theme = createTheme(
-    _.merge({}, baseOptions, themeOptions[0], { direction: config.direction })
-  )
+  let theme = createTheme(options)
   theme = responsiveFontSizes(theme)
 
   return theme

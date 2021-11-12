8956da46e12d19af1c577ea0bd91f86e777b0f48
chengmark <mocheng26@gmail.com>
2021-11-12 19:23:09 +0800

    first commit for this bramch

--- daemon/backup.sh
+++
@@ -1,6 +0,0 @@
-# decided to not use -m flag, as it consumes networks very much
-# https://cloud.google.com/storage/docs/gsutil/addlhelp/TopLevelCommandLineOptions
-# gsutil -m rsync -r /home/s1155144832/server/world/ gs://mc-cuhk-server-backup/world/
-gsutil rsync -r /home/s1155144832/server/world/ gs://mc-cuhk-server-backup/world/ &&
-  gsutil rsync -r /home/s1155144832/server/world_nether/ gs://mc-cuhk-server-backup/world_nether/ &&
-  gsutil rsync -r /home/s1155144832/server/world_the_end/ gs://mc-cuhk-server-backup/world_the_end/
